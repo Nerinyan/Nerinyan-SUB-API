3a76a39775c5f49fce30331e02d22569acce602f
ZEEE <j1nd2su@gmail.com>
2022-10-24 22:02:09 +0900

    A

--- run.py
+++ run.py
@@ -12,8 +12,9 @@ else:
 os.system("clear" if platform.system() == "Linux" else "cls")
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--port', help='Specify the port on which to run the server. (Default is 8000)')
+parser.add_argument('--port', help='Specify the port on which to run the server. (Default is 8000)', default="8000", type=str)
+parser.add_argument('--host', help='Specify the host address on which to run the server. (Default is 127.0.0.1)', default="127.0.0.1", type=str)
 parser.add_argument('--dev', help='Auto Reload', default=0, type=bool)
 args = parser.parse_args()
 
-os.system(f"uvicorn main:app {'--reload' if args.dev else ''} --port={8000 if args.port == None else args.port}")
+os.system(f"uvicorn main:app {'--reload' if args.dev else ''} --port={args.port} --host={args.host}")
